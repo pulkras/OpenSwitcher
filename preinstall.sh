0325557de9464ac1153ab822abe0bbfc7f5254f4
Ruslan <gald.rus@yandex.ru>
2023-09-13 18:15:31 +0300

    improve preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -27,37 +27,83 @@ for package in "${required_packages[@]}"; do
     fi
 done
 
+add_standart_combination() 
+{
+    if [ "$1" -eq 1 ]; then
+        if ! grep -q "$defaultCombination" "$file"; then
+            read -r -p "Хотите добавить стандартную комбинацию клавиш в файл ? (y/n): " answer
+            if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+                echo "Добавляется стандартная комбинация клавиш для запуска"
+                echo "$defaultCombination" >> "$HOME/.xbindkeysrc"
+                echo "Установите необходимую вам конфигурацию запуска"
+            else
+                echo "стандартная комбинация клавиш не добавлена"
+            fi
+        else
+            echo "Стандартная комбинация клавиш найдена"
+        fi
+    else
+        read -r -p "Хотите добавить стандартный $file ? (y/n): " answer
+        if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+            echo "Установка стандартного файла $file"
+            xbindkeys --defaults > "$file"
+            echo "Добавляется стандартная комбинация клавиш для запуска"
+            echo "$defaultCombination" >> "$HOME/.xbindkeysrc"
+            echo "Установите необходимую вам конфигурацию запуска"
+        else
+            echo "Error: для работы программы необходим $file" >> /dev/stderr
+            exit 1
+        fi
+    fi
+    
+}
+
+add_standart_autostart()
+{   
+    if [ "$1" -eq 1 ]; then
+        if ! grep -q "exec xbindkeys&" "$file"; then
+            echo "Рекомендуется добавить автостарт xbindkeys при запуску X session"
+            read -r -p "Хотите добавить автостарт xbindkeys в $file? (y/n): " answer
+            if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+                echo "exec xbindkeys&" >> "$HOME"/.xinitrc
+            else
+                echo "Автостарт xbindkeys не добавлен"
+            fi
+        else
+            echo "Стандартный автостарт найден"
+        fi
+    else
+        echo "Рекомендуется добавить автостарт xbindkeys при запуску X session"
+        read -r -p "Хотите добавить автостарт xbindkeys в $file? (y/n): " answer
+        if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+            cp /etc/X11/xinit/xinitrc "$HOME"/.xinitrc
+            echo "exec xbindkeys&" >> "$HOME"/.xinitrc
+        else
+            echo "Автостарт xbindkeys не добавлен"
+        fi
+    fi
+    
+}
+
 # Check requested files
 for file in "${required_files[@]}"; do
     if [ ! -f "$file" ]; then
         echo "Файл $file не найден."
 
         if [ "$file" = "$HOME/.xbindkeysrc" ]; then
-
-            read -r -p "Хотите добавить стандартный $file ? (y/n): " answer
-            if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
-                echo "Установка стандартного файла $file"
-                xbindkeys --defaults > "$file"
-                echo "Добавляется стандартная комбинация клавиш для запуска"
-                echo "$defaultCombination" >> "$HOME/.xbindkeysrc"
-                echo "Установите необходимую вам конфигурацию запуска"
-            else
-                echo "Error: для работы программы необходим $file" >> /dev/stderr
-                exit 1
-            fi
-            
+            add_standart_combination 0
         elif [ "$file" = "$HOME/.xinitrc" ]; then
-            echo "Рекомендуется добавить автостарт xbindkeys при запуску X session"
-            read -r -p "Хотите добавить автостарт xbindkeys в $file? (y/n): " answer
-            if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
-                cp /etc/X11/xinit/xinitrc "$HOME"/.xinitrc
-                echo "exec xbindkeys&" >> "$HOME"/.xinitrc
-            else
-                echo "Автостарт xbindkeys не добавлен"
-            fi
+            add_standart_autostart 0
         fi
     else
         echo "Файл $file найден."
+
+        if [ "$file" = "$HOME/.xbindkeysrc" ]; then
+            add_standart_combination 1
+        elif [ "$file" = "$HOME/.xinitrc" ]; then
+            add_standart_autostart 1
+        fi
+       
     fi
 done
 
